0f2744ac0c0e01f28cceba7e85234860cbf4f3e4
Pieterjan Vandenweghe <Pieterjan.vandenweghe@flowtime.be>
2019-11-29 15:36:29 +0100

    Review PJ

---
+++ _output/webhelp_responsive/__flow_dita_webhelp.bookmap/oxygen-webhelp/app/nav-links/json/co_what_is_a_dita_map-d62e1011.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Purposes and advantages of DITA maps","href":"en\/co_why_use_dita_maps.html","attributes": {"data-id":"co_why_use_dita_maps",},"menu": {"hasChildren":false,},"tocID":"co_why_use_dita_maps-d62e1051","topics":[]}]});
